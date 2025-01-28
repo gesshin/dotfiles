cb48350e4158ca7878cf6f1fbcd49de6152fd5f3
RyanEweSeng <ryantoh1001@gmail.com>
2025-01-28 12:54:57 -0500

    update nvim config

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -17,6 +17,7 @@ return {
         end
       },
       mapping = {
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<CR>'] = cmp.mapping.confirm(),
