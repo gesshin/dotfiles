638333e2f2ef5a76908c99a5aecc154c80cbc466
RyanEweSeng <ryantoh1001@gmail.com>
2025-02-19 15:14:22 -0500

    update tmux scripts

--- tmux/scripts/batt.sh
+++ tmux/scripts/batt.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-BATTERY_LEVEL=$(pmset -g batt | grep -Eo "\d+%")
+BATTERY=$(pmset -g batt)
 
-echo $BATTERY_LEVEL
+LEVEL=$(echo "$BATTERY" | grep -Eo "\d+%")
+STATUS=$(echo "$BATTERY" | grep -Eo "Battery Power|AC Power")
+
+if [[ "$STATUS" == "Battery Power" ]]; then
+  STATUS=""
+elif [[ "$STATUS" == "AC Power" ]]; then
+  STATUS=""
+fi
+
+echo $STATUS $LEVEL
--- tmux/scripts/wttr.sh
+++ tmux/scripts/wttr.sh
@@ -5,6 +5,6 @@ LOCATION=""
 WEATHER=$(curl -s "wttr.in/${LOCATION}?format=%C\n%t&m")
 
 CONDITION=$(echo "$WEATHER" | sed -n '1p')
-TEMP=$(echo "$WEATHER" | sed -n '2p')
+TEMPERATURE=$(echo "$WEATHER" | sed -n '2p')
 
-echo $TEMP $CONDITION
+echo $TEMPERATURE $CONDITION
