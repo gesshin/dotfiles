514f39c02ad6b54bc6f67557f335a54fa945520c
RyanEweSeng <ryantoh1001@gmail.com>
2025-02-19 23:33:40 -0500

    update nvim config

---
+++ nvim/lua/core/highlights.lua
@@ -0,0 +1,76 @@
+local themes = {
+  dracula = require('core.themes').dracula,
+  everforest = require('core.themes').everforest,
+}
+
+local set_hl = vim.api.nvim_set_hl
+
+local function set_colorscheme(colorscheme)
+  vim.cmd.colorscheme(colorscheme)
+end
+
+local function set_lualine(theme)
+  require('lualine').setup({ options = { theme = theme } })
+end
+
+local function set_highlights(palette)
+  -- Theme Highlights
+  set_hl(0, 'ThemeBg'     , { fg = palette.bg      })
+  set_hl(0, 'ThemeFg'     , { fg = palette.fg      })
+  set_hl(0, 'ThemeLine'   , { fg = palette.line    })
+  set_hl(0, 'ThemeComment', { fg = palette.comment })
+  set_hl(0, 'ThemeRed'    , { fg = palette.red     })
+  set_hl(0, 'ThemeOrange' , { fg = palette.orange  })
+  set_hl(0, 'ThemeYellow' , { fg = palette.yellow  })
+  set_hl(0, 'ThemeGreen'  , { fg = palette.green   })
+  set_hl(0, 'ThemeBlue'   , { fg = palette.blue    })
+  set_hl(0, 'ThemePurple' , { fg = palette.purple  })
+  set_hl(0, 'ThemePink'   , { fg = palette.pink    })
+
+  -- Alpha Highlights
+  set_hl(0, 'AlphaHeader'  , { fg = palette.fg  })
+  set_hl(0, 'AlphaFooter'  , { fg = palette.red })
+  set_hl(0, 'AlphaButtons' , { fg = palette.fg  })
+  set_hl(0, 'AlphaShortcut', { fg = palette.red })
+
+  -- WhichKey Highlights
+  set_hl(0, 'WhichKeyNormal'   , { fg = palette.fg      })
+  set_hl(0, 'WhichKeyTitle'    , { fg = palette.purple  })
+  set_hl(0, 'WhichKeyBorder'   , { fg = palette.comment })
+  set_hl(0, 'WhichKey'         , { fg = palette.purple  })
+  set_hl(0, 'WhichKeySeparator', { fg = palette.comment })
+  set_hl(0, 'WhichKeyIcon'     , { fg = palette.purple  })
+  set_hl(0, 'WhichKeyDesc'     , { fg = palette.fg      })
+  set_hl(0, 'WhichKeyGroup'    , { fg = palette.fg      })
+
+  -- NvimCmp Highlights
+  set_hl(0, 'FloatBorder'          , { fg = palette.fg      })
+  set_hl(0, 'PmenuThumb'           , { bg = palette.comment })
+  set_hl(0, 'CmpItemAbbr'          , { fg = palette.comment })
+  set_hl(0, 'CmpItemAbbrDeprecated', { fg = palette.comment })
+  set_hl(0, 'CmpItemAbbrMatch'     , { fg = palette.blue    })
+  set_hl(0, 'CmpItemAbbrMatchFuzzy', { fg = palette.blue    })
+  set_hl(0, 'CmpItemMenu'          , { fg = palette.comment })
+
+  -- Diagnostic Highlights
+  set_hl(0, 'DiagnosticSignError', { fg = palette.red    })
+  set_hl(0, 'DiagnosticSignWarn' , { fg = palette.yellow })
+  set_hl(0, 'DiagnosticSignInfo' , { fg = palette.blue   })
+  set_hl(0, 'DiagnosticSignHint' , { fg = palette.blue   })
+  local diagnostic_signs = { Error = '', Warn = '', Info = '', Hint = '' }
+  for type, icon in pairs(diagnostic_signs) do
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+  end
+end
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = vim.api.nvim_create_augroup('Colorscheme', {}),
+  callback = function()
+    local theme = vim.env.GLOBAL_THEME
+
+    set_colorscheme(theme)
+    set_highlights(themes[theme])
+    set_lualine(theme)
+  end
+})
--- nvim/lua/core/themes.lua
+++ nvim/lua/core/themes.lua
@@ -1,76 +1,31 @@
-local palettes = {
-  dracula = require('core.palettes').dracula,
-  everforest = require('core.palettes').everforest,
+local M = {}
+
+M.dracula = {
+  bg      = '#282a36',
+  fg      = '#f8f8f2',
+  line    = '#44475a',
+  comment = '#6272a4',
+  red     = '#ff5555',
+  orange  = '#ffb86c',
+  yellow  = '#f1fa8c',
+  green   = '#50fa7b',
+  blue    = '#8be9fd',
+  purple  = '#bd93f9',
+  pink    = '#ff79c6',
 }
 
-local set_hl = vim.api.nvim_set_hl
+M.everforest = {
+  bg      = '#2d353b',
+  fg      = '#d3c6aa',
+  line    = '#343f44',
+  comment = '#859289',
+  red     = '#e67e80',
+  orange  = '#e69875',
+  yellow  = '#dbbc7f',
+  green   = '#a7c080',
+  blue    = '#7fbbb3',
+  purple  = '#d699b6',
+  pink    = '#83c092',
+}
 
-local function set_colorscheme(colorscheme)
-  vim.cmd.colorscheme(colorscheme)
-end
-
-local function set_lualine(colorscheme)
-  require('lualine').setup({ options = { theme = colorscheme } })
-end
-
-local function set_highlights(palette)
-  -- Theme Highlights
-  set_hl(0, 'ThemeBg'     , { fg = palette.black   })
-  set_hl(0, 'ThemeFg'     , { fg = palette.white   })
-  set_hl(0, 'ThemeLine'   , { fg = palette.line    })
-  set_hl(0, 'ThemeComment', { fg = palette.comment })
-  set_hl(0, 'ThemeRed'    , { fg = palette.red     })
-  set_hl(0, 'ThemeOrange' , { fg = palette.orange  })
-  set_hl(0, 'ThemeYellow' , { fg = palette.yellow  })
-  set_hl(0, 'ThemeGreen'  , { fg = palette.green   })
-  set_hl(0, 'ThemeBlue'   , { fg = palette.blue    })
-  set_hl(0, 'ThemePurple' , { fg = palette.purple  })
-  set_hl(0, 'ThemePink'   , { fg = palette.pink    })
-
-  -- Alpha Highlights
-  set_hl(0, 'AlphaHeader'  , { fg = palette.fg  })
-  set_hl(0, 'AlphaFooter'  , { fg = palette.red })
-  set_hl(0, 'AlphaButtons' , { fg = palette.fg  })
-  set_hl(0, 'AlphaShortcut', { fg = palette.red })
-
-  -- WhichKey Highlights
-  set_hl(0, 'WhichKeyNormal'   , { fg = palette.fg      })
-  set_hl(0, 'WhichKeyTitle'    , { fg = palette.purple  })
-  set_hl(0, 'WhichKeyBorder'   , { fg = palette.comment })
-  set_hl(0, 'WhichKey'         , { fg = palette.purple  })
-  set_hl(0, 'WhichKeySeparator', { fg = palette.comment })
-  set_hl(0, 'WhichKeyIcon'     , { fg = palette.purple  })
-  set_hl(0, 'WhichKeyDesc'     , { fg = palette.fg      })
-  set_hl(0, 'WhichKeyGroup'    , { fg = palette.fg      })
-
-  -- NvimCmp Highlights
-  set_hl(0, 'FloatBorder'          , { fg = palette.fg      })
-  set_hl(0, 'PmenuThumb'           , { bg = palette.comment })
-  set_hl(0, 'CmpItemAbbr'          , { fg = palette.comment })
-  set_hl(0, 'CmpItemAbbrDeprecated', { fg = palette.comment })
-  set_hl(0, 'CmpItemAbbrMatch'     , { fg = palette.blue    })
-  set_hl(0, 'CmpItemAbbrMatchFuzzy', { fg = palette.blue    })
-  set_hl(0, 'CmpItemMenu'          , { fg = palette.comment })
-
-  -- Diagnostic Highlights
-  set_hl(0, 'DiagnosticSignError', { fg = palette.red    })
-  set_hl(0, 'DiagnosticSignWarn' , { fg = palette.yellow })
-  set_hl(0, 'DiagnosticSignInfo' , { fg = palette.blue   })
-  set_hl(0, 'DiagnosticSignHint' , { fg = palette.blue   })
-  local diagnostic_signs = { Error = '', Warn = '', Info = '', Hint = '' }
-  for type, icon in pairs(diagnostic_signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-  end
-end
-
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = vim.api.nvim_create_augroup('Colorscheme', {}),
-  callback = function()
-    local colorscheme = vim.env.GLOBAL_THEME
-
-    set_colorscheme(colorscheme)
-    set_highlights(palettes[colorscheme])
-    set_lualine(colorscheme)
-  end
-})
+return M
