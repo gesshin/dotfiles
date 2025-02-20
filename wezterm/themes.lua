efb74d381ebdd9d02f548b6409c3a3b1013551bf
RyanEweSeng <ryantoh1001@gmail.com>
2025-02-20 03:39:00 -0500

    add wezterm config

---
+++ wezterm/themes.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.dracula = {
+  background    = '#282a36',
+  foreground    = '#f8f8f2',
+  cursor_bg     = '#f8f8f2',
+  cursor_fg     = '#282a36',
+  cursor_border = '#f8f8f2',
+  ansi          = { '#44475a', '#ff5555', '#50fa7b', '#ffb86c', '#8be9fd', '#bd93f9', '#ff79c6', '#f8f8f2' },
+  brights       = { '#6272a4', '#ff5555', '#50fa7b', '#ffb86c', '#8be9fd', '#bd93f9', '#ff79c6', '#ffffff' },
+}
+
+M.everforest = {
+  background    = '#2d353b',
+  foreground    = '#d3c6aa',
+  cursor_bg     = '#d3c6aa',
+  cursor_fg     = '#2d353b',
+  cursor_border = '#d3c6aa',
+  ansi          = { '#343f44', '#e67e80', '#a7c080', '#dbbc7f', '#7fbbb3', '#d699b6', '#83c092', '#d3c6aa' },
+  brights       = { '#859289', '#e67e80', '#a7c080', '#dbbc7f', '#7fbbb3', '#d699b6', '#83c092', '#dfddc8' },
+}
+
+return M
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require('wezterm')
+local themes = require('themes')
+
+local config = wezterm.config_builder()
+
+config.colors = themes[os.getenv('GLOBAL_THEME')]
+config.enable_tab_bar = false
+config.font = wezterm.font('JetBrainsMonoNL Nerd Font Mono')
+config.font_size = 12.0
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+config.default_prog = { '/bin/zsh', '-c', 'tmux new-session -A -s default' }
+
+return config
