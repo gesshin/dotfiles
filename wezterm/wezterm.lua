d00c49342693952c6204056f3e6db1f3a9ec1bb8
RyanEweSeng <ryantoh1001@gmail.com>
2025-02-20 19:06:22 -0500

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,4 @@ config.font = wezterm.font('JetBrainsMonoNL Nerd Font Mono')
 config.font_size = 12.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
--- Wezterm runs shells as login shells by default, which affects the config files the shell loads
-config.default_prog = { 'zsh', '-l', '-c', 'tmux new-session -A -s default' }
-
 return config
