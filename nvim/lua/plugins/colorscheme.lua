4b482fd5d6b24bf829de16dcb08d675ddce22757
rtoh_LINKS <rtoh@linksquares.com>
2025-08-20 13:04:18 -0400

    set fixed commit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   {
     'Mofiqul/dracula.nvim',
+    commit = 'df528c8',
     lazy = false,
     priority = 1000
   },
   {
     'neanias/everforest-nvim',
+    commit = '8db2bd8',
     lazy = false,
     priority = 1000
   },
