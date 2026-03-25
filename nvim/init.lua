bdcfbb48a7c0c03c63ee910b5ede976327ead986
gesshin <ryantoh1001@gmail.com>
2026-03-24 21:34:04 -0400

    chore(neovim): clean up init formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-require('core.lazy')
-require('core.options')
-require('core.keymaps')
-require('core.highlights')
+require("core.lazy")
+require("core.options")
+require("core.keymaps")
+require("core.highlights")
