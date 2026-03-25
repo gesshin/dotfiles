3487d1d350b7a7550ce04f3c650ba62c2f671492
gesshin <ryantoh1001@gmail.com>
2026-03-25 16:34:11 -0400

    chore(neovim): formatting

--- nvim/lua/core/highlights/mappings.lua
+++ nvim/lua/core/highlights/mappings.lua
@@ -49,7 +49,7 @@ M.flash = {
 
 M.float = {
 	NormalFloat = { bg = "bg" },
-  FloatTitle = { fg = "orange", bg = "bg" },
+	FloatTitle = { fg = "orange", bg = "bg" },
 	FloatBorder = { fg = "fg" },
 }
 
